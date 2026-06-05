23e318b9ceed09cf08a12162e3742ab8ba61056f
debian_vm <yoavcohen5@mail.tau.ac.il>
2026-06-05 11:38:38 +0300

    - Added the > and < parts - Added a few includes - changed the structure of the process_arglist   and exported most of it to functions

--- hw_3/shell.c
+++ hw_3/shell.c
@@ -4,7 +4,10 @@
 #include <string.h>
 #include <errno.h>
 #include <signal.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 // arglist - a list of char* arguments (words) provided by the user
 // it contains count+1 items, where the last item (arglist[count]) and *only* the last is NULL
@@ -15,87 +18,193 @@ int process_arglist(int count, char** arglist);
 int prepare(void);
 int finalize(void);
 
-// Identify which case we need to handle
-int case_identify(int count, char** arglist){
-
-	int i;	
-	if((strcmp(arglist[count - 1], "&")) == 0){
-		return 1;
-	}
-	if(count > 1){
-		if((strcmp(arglist[count - 2], "<")) == 0){
-			return 2;
-		}
-		if((strcmp(arglist[count - 2], ">")) == 0){
-			return 3;
-		}
-	}
-	for(i = 0; i < count; i++){
-		if((strcmp(arglist[i], "|")) == 0){
-			return 4;
-		}
-	}
-	return 0;
-}
-
 int prepare(void){
 	signal(SIGINT, SIG_IGN);
 	return 0;
 }
 
-
-
-int process_arglist(int count, char** arglist){
-
+/**
+ * @brief 
+ * Executes a process according to the given input
+ * @param count the number of non NULL input words
+ * @param arglist the string of input
+ * @return int returns 0 on failure and 1 on success
+ */
+int execute_default(int count, char** arglist){
 	int pid, status;
-	
-	switch(case_identify(count, arglist)){
-		case 1:
-		// &
-		pid = fork();
-		// Handle unsuccessful fork
-		if(pid == -1){
-			perror("Failed to launch background process");
-			return 0;
-		}
-		if(pid == 0){
-			arglist[count - 1] = NULL;
-			if(execvp(arglist[0], arglist) == -1){
-				perror("Failed to execute process");
-				exit(1);
-			}
-		}
-		return 1;
-
-		case 2:
-		// <
-		break;
-		case 3:
-		// >
-		break;
-		case 4:
-		// |
-		break;
-		default:
-		//No special case
-		pid = fork();
-		if(pid == -1){
-			perror("Failed to execute process");
-			return 0;
-		}
-	
-		if (pid == 0){
-			// Changing the signal such that the child will terminate upon SIGINT
-			signal(SIGINT, SIG_DFL);
-			if(execvp(arglist[0], arglist) == -1){
-				perror("Failed to execute process");
-				exit(1);
-			}
-		}
-		waitpid(pid, &status, 0);
-		return 1;
+	pid = fork();
+	if(pid == -1){
+		perror("Failed to execute process");
+		return 0;
 	}
 
+	if (pid == 0){
+		// Changing the signal such that the child will terminate upon SIGINT
+		signal(SIGINT, SIG_DFL);
+		if(execvp(arglist[0], arglist) == -1){
+			perror("Failed to execute process");
+			exit(1);
+		}
+	}
+	waitpid(pid, &status, 0);
+	return 1;
+}
+
+
+
+
+/**
+ * @brief 
+ * Executes a background process according to the given input
+ * @param count the number of non NULL input words
+ * @param arglist the string of input
+ * @return int returns 0 on failure and 1 on success
+ */
+int execute_background(int count, char** arglist){
+	int pid;
+	pid = fork();
+	// Handle unsuccessful fork
+	if(pid == -1){
+		perror("Failed to launch background process");
+		return 0;
+	}
+	if(pid == 0){
+		// Updating the input and executing the process
+		arglist[count - 1] = NULL;
+		if(execvp(arglist[0], arglist) == -1){
+			perror("Failed to execute process");
+			exit(1);
+		}
+	}
+	return 1;
+
+}
+
+/**
+ * @brief 
+ * Executes a process where the input is redirected from a given file\
+ * according to the given input
+ * @param count the number of non NULL input words
+ * @param arglist the string of input
+ * @return int returns 0 on failure and 1 on success
+ */
+int execute_input_redirection(int count, char** arglist){
+	int pid, status, fd;
+	char* file_path;
+
+	pid = fork();
+	if(pid == -1){
+		perror("Failed to launch background process");
+		return 0;
+	}
+	if(pid == 0){
+		// Changing the signal such that the child will terminate upon SIGINT
+		signal(SIGINT, SIG_DFL);
+
+		// Setting the given file path as the input location
+		file_path = arglist[count - 1];
+		fd = open(file_path, O_RDONLY, S_IRUSR | S_IWUSR);
+		if(fd == -1){
+			perror("Failed to open given file path");
+			exit(1);
+		}
+		if(dup2(fd, STDIN_FILENO) == -1){
+			perror("Failed to redirect input from given file");
+			close(fd);
+			exit(1);
+		}
+		close(fd);
+
+		// Updating the input and executing the process 
+		arglist[count - 2] = NULL;
+			if(execvp(arglist[0], arglist) == -1){
+				perror("Failed to execute process");
+				exit(1);
+			}
+	}
+	waitpid(pid, &status, 0);
+		return 1;
+}
+
+/**
+ * @brief 
+ * Executes a process where the output is redirected to a given file\
+ * according to the given input
+ * @param count the number of non NULL input words
+ * @param arglist the string of input
+ * @return int returns 0 on failure and 1 on success
+ */
+int execute_output_redirection(int count, char** arglist){
+	int pid, status, fd;
+	char* file_path;
+
+	pid = fork();
+	if(pid == -1){
+		perror("Failed to launch background process");
+		return 0;
+	}
+	if(pid == 0){
+		// Changing the signal such that the child will terminate upon SIGINT
+		signal(SIGINT, SIG_DFL);
+
+		// Setting the given file path as the output location
+		file_path = arglist[count - 1];
+		fd = open(file_path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if(fd == -1){
+			perror("Failed to open given file path");
+			exit(1);
+		}
+		if(dup2(fd, STDOUT_FILENO) == -1){
+			perror("Failed to redirect output given path");
+			close(fd);
+			exit(1);
+		}
+		close(fd);
+
+		// Updating the input and executing the process 
+		arglist[count - 2] = NULL;
+			if(execvp(arglist[0], arglist) == -1){
+				perror("Failed to execute process");
+				exit(1);
+			}
+	}
+	waitpid(pid, &status, 0);
+		return 1;
+}
+
+/**
+ * @brief 
+ * Executes a process according to the given input\
+ * where piping is needed 
+ * @param count the number of non NULL input words
+ * @param arglist the string of input
+ * @return int returns 0 on failure and 1 on success
+ */
+int execute_piping(int count, char** arglist){
+	return 1;
+}
+int process_arglist(int count, char** arglist){
+
+	int i;
+	if((strcmp(arglist[count - 1], "&")) == 0){
+		return execute_background(count, arglist);
+	}
+	if(count > 1){
+		if((strcmp(arglist[count - 2], "<")) == 0){
+			return execute_input_redirection(count, arglist);		
+		}
+		if((strcmp(arglist[count - 2], ">")) == 0){
+			return execute_output_redirection(count, arglist);
+		}
+	}
+	for(i = 0; i < count; i++){
+		if((strcmp(arglist[i], "|")) == 0){
+			return execute_piping(count, arglist);
+		}
+	}
+
+	//No special case
+	return execute_default(count, arglist);
 }
 
 int main(void)
